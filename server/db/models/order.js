636dd247db6622cd8161e50311ecab17532d0b99
Andrew O'Grady <andrewogrady3@gmail.com>
2019-09-18 16:00:44 -0400

    resolve merge conflicts

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,18 +2,9 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Order = db.define('order', {
-  quantity: {
-    type: Sequelize.INTEGER
-  },
   isPurchased: {
     type: Sequelize.BOOLEAN,
     defaultValue: false
-  },
-  orderNumber: {
-    type: Sequelize.INTEGER
-  },
-  price: {
-    type: Sequelize.INTEGER
   },
   orderDate: {
     type: Sequelize.DATE,
