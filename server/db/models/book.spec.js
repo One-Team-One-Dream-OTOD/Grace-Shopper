c486d638d27edbfd70b0287e63725095b025f96e
Tony Wong <wongtonyb@gmail.com>
2019-09-19 12:03:15 -0400

    book model testspec

--- server/db/models/book.spec.js
+++ server/db/models/book.spec.js
@@ -5,6 +5,8 @@ const {expect} = require('chai')
 const should = require('chai').should()
 const db = require('../index')
 const Book = db.model('book')
+const Genre = db.model('genre')
+const Author = db.model('author')
 
 chai.use(require('chai-integer'))
 
@@ -16,7 +18,14 @@ describe('Book Model', () => {
 
   describe('Instance Test', () => {
     let ctci
+    let genre
+    let author
     beforeEach(async () => {
+      genre = await Genre.create({genre: 'education'})
+      author = await Author.create({
+        firstName: 'Gayle L.',
+        lastName: 'McDowell'
+      })
       ctci = await Book.create({
         name: 'Cracking the Coding Interview',
         description:
@@ -24,23 +33,19 @@ describe('Book Model', () => {
         imageUrl:
           'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRCaFEwuavSwsInA7rRVPbU7iUU4sTLBif7SoTQnVghTBNld3FxU8YJk5sFYmQyjPM-eN0vM4Xbz2mBHSa4mJtWH1UFM0PeNACFsxPKT9Sct1ihhrRgjOeD2Q&usqp=CAE',
         price: 4000,
-        SKU: 300
+        SKU: 300,
+        authorId: 1,
+        genreId: 1
       })
     })
 
-    // it('contains all necessary properties', () => {
-    //   ctci.should.have.keys('name', 'description', 'imageUrl', 'price', 'SKU')
-    // })
-
-    // it('contains all necessary properties', () => {
-    //   expect(ctci).to.have.keys([
-    //     'name',
-    //     'description',
-    //     'imageUrl',
-    //     'price',
-    //     'SKU'
-    //   ])
-    // })
+    it('contains all necessary properties', () => {
+      expect(ctci.dataValues).to.have.property('name')
+      expect(ctci.dataValues).to.have.property('description')
+      expect(ctci.dataValues).to.have.property('imageUrl')
+      expect(ctci.dataValues).to.have.property('price')
+      expect(ctci.dataValues).to.have.property('SKU')
+    })
 
     it('name is a string', () => {
       expect(ctci.name).to.be.a('string')
