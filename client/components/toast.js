2d1f549f21fa6e9f034cfd385d8141cf1e960f7e
Andrew O'Grady <andrewogrady3@gmail.com>
2019-09-24 12:52:22 -0400

    set timeout added to toast

--- client/components/toast.js
+++ client/components/toast.js
@@ -1,14 +1,25 @@
 import React, {Component} from 'react'
 
 class Toast extends Component {
+  componentDidMount() {
+    setTimeout(() => {
+      this.props.onDismissClick()
+    }, 5000)
+  }
+
   render() {
     return (
-      <li className="toast" style={{backgroundColor: this.props.color}}>
-        <p className="toast__content">{this.props.text}</p>
-        <button className="toast__dismiss" onClick={this.props.onDismissClick}>
-          x
-        </button>
-      </li>
+      <React.Fragment>
+        <li className="toast" style={{backgroundColor: this.props.color}}>
+          <p className="toast__content">{this.props.text}</p>
+          <button
+            className="toast__dismiss"
+            onClick={this.props.onDismissClick}
+          >
+            x
+          </button>
+        </li>
+      </React.Fragment>
     )
   }
 
