8feeaaf4a2dea5ea38c8a2453ad8e63929d8ab9b
Andrew O'Grady <andrewogrady3@gmail.com>
2019-09-24 16:51:08 -0400

    fix bug

--- client/components/toasts.js
+++ client/components/toasts.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {removeToast} from '../store/toast'
-import Toast from './Toast'
+import Toast from './toast'
 
 const Toasts = props => {
   return (
