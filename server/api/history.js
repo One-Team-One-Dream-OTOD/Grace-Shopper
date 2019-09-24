ee97268c476b0ca592f080fa8e032a4b0d1228a2
Tony Wong <wongtonyb@gmail.com>
2019-09-24 15:02:35 -0400

    order-history

--- server/api/history.js
+++ server/api/history.js
@@ -3,30 +3,6 @@ const {Book, Order, OrderProduct} = require('../db/models/')
 module.exports = router
 
 // GET: /api/history/
-// router.get('/', async (req, res, next) => {
-//   if (req.user) {
-//     try {
-//       const orders = await OrderProduct.findAll({
-//         include: [
-//           {
-//             model: Order,
-//             where: {
-//               userId: req.user.id,
-//               isPurchased: true
-//             }
-//           },
-//           Book
-//         ]
-//       })
-//       res.json(orders)
-//     } catch (error) {
-//       next(error)
-//     }
-//   } else {
-//     res.status(404).json()
-//   }
-// })
-
 router.get('/', async (req, res, next) => {
   if (req.user) {
     try {
